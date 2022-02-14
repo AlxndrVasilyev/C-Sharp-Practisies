514f3816cbecf0497886b4e6bf51c3d72dfab096
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-14 19:16:35 +0300

    wrote task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine("Это число чётное");
+} 
+else
+{
+    Console.WriteLine("Это число нечётное");
+}
