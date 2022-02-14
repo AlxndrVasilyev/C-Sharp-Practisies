2cd9562b67690bd32a174f963f3c437fe1a1446c
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-14 18:51:17 +0300

    wrote task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Генерирую три случайных числа: ");
+
+int n1 = new Random().Next(1,5);
+int n2 = new Random().Next(1,5);
+int n3 = new Random().Next(1,5);
+
+Console.WriteLine("Первое число: " + n1);
+Console.WriteLine("Второе число: " + n2);
+Console.WriteLine("Третье число: " + n3);
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine(n1 + " - максимальное число");
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine(n2 + " - максимальное число");
+}
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine(n3 + " - максимальное число");
+}
+else if (n1 == n2 && n1 == n3)
+{
+    Console.WriteLine("Максимального числа нет, все равны");
+}
+else if (n1 == n2)
+{
+    Console.WriteLine("Максимальное число повторяется и равно" + n1);
+}
+else if (n1 == n3)
+{
+    Console.WriteLine("Максимальное число повторяется и равно" + n1);
+}
