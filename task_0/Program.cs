c8a825763312236144a1b4243d2fe9047b9ec36b
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-14 17:38:54 +0300

    wrote task_0

---
+++ task_0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести квадрат числа
+
+Console.WriteLine("Введите число: ");
+
+// int n = new Random().Next(1,10); - генератор случайных чисел от 1 до 9
+
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("n = " + n); // - вывод числа из генератора
+Console.WriteLine("nˆ2 = " + n*n);
