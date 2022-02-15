45a8aabe60caeaf193d6a4548ec766290f544651
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-15 23:20:40 +0300

    wrote task_9

---
+++ task_9/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+// Первый способ обычный
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// int n = int.Parse(Console.ReadLine());
+// int lastNumber = -1;
+// lastNumber = n%10;
+// Console.WriteLine("Последняя цифра трёхзначного числа - " + lastNumber);
+
+
+// теперь с помощью функции(метода)
+
+int LastDigit(int Number)
+{
+    return Number % 10;
+}
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Последняя цифра трёхзначного числа - " + LastDigit(n));
