241e9ba5beded1933fe75f7ebbeafa82cd24962e
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-16 00:44:09 +0300

    upgraded task_11

--- task_11/Program.cs
+++ task_11/Program.cs
@@ -1,24 +1,14 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
-
-int n = new Random().Next(10, 100);
-Console.WriteLine("Сгенерированное число: " + n);
 int MaxOfTwo(int max)
 {
     int firstDigit = max % 10;
     int secondDigit = max / 10;
-    int result = -1;
-    if (firstDigit > secondDigit)
-    {
-        result = firstDigit;
-    }
-    else if (secondDigit > firstDigit)
-    {
-        result = secondDigit;
-    }
-    else
-    {
-        Console.WriteLine("Наибольшей цифры нет");
-    }
+    int result = firstDigit;
+
+    if (firstDigit > secondDigit) result = firstDigit;
+    else if (secondDigit > firstDigit) result = secondDigit;
 return result;
 }
+int n = new Random().Next(10, 100);
+Console.WriteLine("Сгенерированное число: " + n);
 Console.WriteLine("Наибольшая цифра числа: " + MaxOfTwo(n));
