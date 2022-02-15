c30f672ba4f617a61c05b7f257fa8debdc28d98e
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-15 23:26:15 +0300

    wrote task_9

---
+++ task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int SecondDigit(int Number)
+{
+    return Number / 10 % 10;
+}
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра трёхзначного числа - " + SecondDigit(n));
