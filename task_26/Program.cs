a0378eb25e470b6f3b144a41c854051d58c99921
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-16 22:39:51 +0300

    almostdone task_26

---
+++ task_26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Возведите число А в натуральную степень B, используя цикл.
+Console.WriteLine("Задайте число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте степень: ");
+int B = int.Parse(Console.ReadLine());
+int I = 2;
+int result = A * A;
+while (B > I)
+{
+    result = result * A;
+    I++;
+}
+Console.WriteLine(result);
