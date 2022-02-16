0a3ced9bc6c3dc86c563bc8670788d762a9676d3
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-16 14:19:05 +0300

    rewrote task_13

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -4,6 +4,12 @@ Console.WriteLine("Задайте первое число: ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Задайте второе число: ");
 int b = int.Parse(Console.ReadLine());
-int Div = a % b;
-if (Div == 0) Console.WriteLine(a + " кратно " + b);
-else Console.WriteLine(a + " не кратно " + b + " и остаток от деления = " + Div);
+if (b == 0) Console.WriteLine("На ноль делить нельзя, перезапустите программу");
+else
+{
+    int Div = a % b;
+    {
+        if (Div == 0) Console.WriteLine(a + " кратно " + b);
+        else Console.WriteLine(a + " не кратно " + b + " и остаток от деления на целое = " + Div);
+    }
+}
