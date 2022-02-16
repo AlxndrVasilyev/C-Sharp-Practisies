f71ebd70d099886f650657cc833fd13c9b97d2a9
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-16 15:32:19 +0300

    wrote task_15

---
+++ task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+if (n%7 == 0 && n%23 == 0) Console.WriteLine(true);
+else Console.WriteLine (false);
+
+// switch (n % 7)
+// {
+//     case 0 when n % 23 == 0:
+//         Console.WriteLine(true);
+//         break;
+//     default:
+//         Console.WriteLine(false);
+//         break;
+// }
