89b4ca6280262c0e027920458643f23e3bc54ad0
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-16 12:54:55 +0300

    wrote task_12

---
+++ task_12/Program.cs
@@ -0,0 +1,17 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int firstDigit(int Number)
+{
+     int firstDigit = Number / 100;
+     return firstDigit;
+}
+int thirdDigit(int Number)
+{
+     int thirdDigit = Number % 10;
+     //return (firstDigit & thirdDigit);
+     return thirdDigit;
+}
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write(firstDigit(n));
+Console.WriteLine(thirdDigit(n));
