f2d33c3f7680e50e06c3ece103730c19f02d7349
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-16 23:34:04 +0300

    almost done task_26

--- task_26/Program.cs
+++ task_26/Program.cs
@@ -3,11 +3,24 @@ Console.WriteLine("Задайте число: ");
 int A = int.Parse(Console.ReadLine());
 Console.WriteLine("Задайте степень: ");
 int B = int.Parse(Console.ReadLine());
-int I = 2;
 int result = A * A;
-while (B > I)
+for (int I = 2; B > I; I++)     
 {
     result = result * A;
-    I++;
 }
 Console.WriteLine(result);
+
+// Console.WriteLine("Задайте число: ");
+// int A = int.Parse(Console.ReadLine());
+// Console.WriteLine("Задайте степень: ");
+// int B = int.Parse(Console.ReadLine());
+
+// int I = 2;
+// int result = A * A;
+
+// while (B > I)
+// {
+//     result = result * A;
+//     I++;
+// }
+// Console.WriteLine(result);
