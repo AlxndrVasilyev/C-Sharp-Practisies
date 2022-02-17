aedd3f9fb23e5fc247e710d5549e9418e9cd0e50
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-17 11:45:12 +0300

    wrote tasks_45 & task_47

---
+++ task_45/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа Фибоначчи
+
+int num = 10;
+int f1 = 1;
+int f2 = 1;
+int result;
+for (int i = 0; i < num; i++)
+{
+    result = f1 + f2;
+    Console.Write(result + " ");
+    f1 = f2;
+    f2 = result;
+}
