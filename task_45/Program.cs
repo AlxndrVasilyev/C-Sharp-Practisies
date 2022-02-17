ec53ebbb016cc6d095a4580132b49572e32b7c9d
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-17 12:40:07 +0300

    some fixes for tasks 45 & 47

--- task_45/Program.cs
+++ task_45/Program.cs
@@ -1,13 +1,25 @@
 ﻿// Показать числа Фибоначчи
 
-int num = 10;
-int f1 = 1;
-int f2 = 1;
-int result;
-for (int i = 0; i < num; i++)
+int a=1;
+int b=1;
+int count=10;
+for (int i = 0; i < count; i++)
 {
-    result = f1 + f2;
-    Console.Write(result + " ");
-    f1 = f2;
-    f2 = result;
+    Console.Write($"{a}, {b}, ");
+    a=a+b;
+    b=b+a;
 }
+
+// ниже альтернатива 
+
+// int num = 10;
+// int f1 = 1;
+// int f2 = 1;
+// int result;
+// for (int i = 0; i < num; i++)
+// {
+//     result = f1 + f2;
+//     Console.Write(result + " ");
+//     f1 = f2;
+//     f2 = result;
+// }
--- task_47/Program.cs
+++ task_47/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Написать программу копирования массива
-void FillArray (int[] arrays)
+void FillArray(int[] arrays)
 {
     for (int i = 0; i < arrays.Length; i++)
-    arrays[i] = new Random().Next(100, 1000);
+        arrays[i] = new Random().Next(100, 1000);
 }
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write(arr[i] + "   ");        
+        Console.Write(arr[i] + "   ");
     }
     Console.WriteLine();
 }
-void CopyArray (int[] arr, int[] arrays)
+void CopyArray(int[] arr, int[] arrays)
 {
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        arrays[i] = arr[i];        
+        arrays[i] = arr[i];
     }
     Console.WriteLine();
 }
