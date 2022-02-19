2623daf9c84d4c9b2ad34efc0177ee1dc888c13f
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 02:38:48 +0300

    wrote task_24

--- task_24/Program.cs
+++ task_24/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Найти кубы чисел от 1 до N
 
-// Показать таблицу квадратов чисел от 1 до N
-
 Console.WriteLine("Задайте число N");
 int N = int.Parse(Console.ReadLine());
 for (int i = 1; i <= N; i++)
