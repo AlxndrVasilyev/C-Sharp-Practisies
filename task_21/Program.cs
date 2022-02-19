20cc6af465dcb7a1f84a5c7dc9ff106bcc815d02
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 02:23:05 +0300

    choice for task_21

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -9,3 +9,11 @@ else if (num[0] == num[4] && num[1] == num[3])
     Console.WriteLine($"Число {num} - палиндром");
 else
     Console.WriteLine($"Число {num} - не палиндром");
+
+// или математический вариант 
+
+// int num = 63136; //new Random().Next(10000,99999);
+// if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
+//     Console.WriteLine($"Число {num} - палиндром");
+// else
+//     Console.WriteLine($"Число {num} - не палиндром");
