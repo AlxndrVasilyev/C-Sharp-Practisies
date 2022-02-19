d1bf7d477d54936a6d41f12f958269b9b24f0043
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-19 21:43:36 +0300

    wrote task_21

---
+++ task_21/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.WriteLine ("Введите пятизначное число");
+string num = Console.ReadLine();
+int length = num.Length;
+if (length != 5) Console.WriteLine ("Вы ввели не пятизначное число. Перезапустите программу");  
+else if (num[0] == num[4] && num[1] == num[3]) Console.WriteLine($"Число {num} - палиндром");
+else Console.WriteLine ($"Число {num} - не палиндром");
