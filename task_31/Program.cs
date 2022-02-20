2c476b8d8b0f249c195ecb99d1e2491d1484821c
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 19:02:25 +0300

    wrote task_31

---
+++ task_31/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int index = 0;
+    while (index < count)
+    {
+        col[index] = new Random().Next(0, 10);
+        Console.Write($"{col[index]} ");
+        index++;
+    }
+}
+int[] col = new int[8];
+Console.WriteLine ($"Случайный массив из 8 элементов: ");
+PrintArray(col);
+Console.WriteLine();
