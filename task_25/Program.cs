10708d4b8e7cc20aad3a4f2f11a0c007b2f5b8e5
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 13:11:01 +0300

    wrote task_25

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,1 +1,11 @@
-﻿// 
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Задайте число: ");
+int A = int.Parse(Console.ReadLine());
+int result = 0;
+
+for (int i = 0; i <= A; i++)
+{
+    result = result + i;
+}
+Console.WriteLine($"Сумма чисел от 1 до {A} = {result}");
