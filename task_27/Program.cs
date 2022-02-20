fcda8049231e9d4cd770a70e99e3d7ccd5b1921c
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 13:36:32 +0300

    wrote task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите любое целое число");
+int num = int.Parse(Console.ReadLine());
+int j = 1;
+for (int i = 0; i < num; i++)
+{
+    num = num/10;
+    j++;
+}
+Console.WriteLine($"В числе содержится {j} цифр");
+
+// ниже через число символов в строке
+
+// Console.WriteLine("Введите любое целое число");
+// string num = Console.ReadLine();  
+// int length = num.Length;
+// Console.WriteLine($"Количество цифр в числе {num} равно {length}");
