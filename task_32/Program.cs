ee021439ba1a233f408da1a0915e859ef1270819
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 19:53:59 +0300

    wrote task_32

---
+++ task_32/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int index = 0;
+    while (index < count)
+    {
+        array[index] = new Random().Next(0, 2);
+        Console.Write($"{array[index]} ");
+        index++;
+    }
+}
+int[] newAarray = new int[8];
+Console.WriteLine($"Массив из 8 элементов заполненный случайными нулями и единицами: ");
+PrintArray(newAarray);
+Console.WriteLine();
