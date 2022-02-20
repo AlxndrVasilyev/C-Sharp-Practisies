ee664c736b0914d0abdfb1b938387b041b414bbe
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 14:21:23 +0300

    wrote task_28

---
+++ task_28/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите любое целое число");
+int num = int.Parse(Console.ReadLine());
+int result = 0;
+while (num != 0)
+{
+    result = result + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {result}");
