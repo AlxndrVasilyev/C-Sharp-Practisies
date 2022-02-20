377ccb77b630f9c0b8f3ce758eef2d9064ec0cb2
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-20 14:47:02 +0300

    wrote task_29

---
+++ task_29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Задайте число: ");
+double N = int.Parse(Console.ReadLine());
+double result = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    result = result * i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} = {result}");
