c9b5f3b1cc6a5168548131b21004311f616d3e12
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-21 12:06:34 +0300

    upgrated task_54

--- task_54/Program.cs
+++ task_54/Program.cs
@@ -1,37 +1,82 @@
 ﻿// В матрице чисел найти сумму элементов главной диагонали
 
-int n = new Random().Next(5, 10);
-int k = new Random().Next(5, 10);
-int[,] array = new int[n, k];
-int a = 0;
+// int n = new Random().Next(5, 10);
+// int k = new Random().Next(5, 10);
+// int[,] array = new int[n, k];
+// int a = 0;
 
-void PrintArray(int[,] arr)
+// void PrintArray(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(0, 10);
+//     }
+// }
+// PrintArray(array);
+// Console.WriteLine();
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         if (i == j) a = a + array[i, j];
+//     }
+// }
+// Console.WriteLine(a);
+
+
+
+void PrintArray(int[,] matr)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            Console.Write(arr[i, j] + " ");
+            Console.Write($" {matr[i, j]}");
         }
         Console.WriteLine();
     }
 }
 
-for (int i = 0; i < array.GetLength(0); i++)
+void FillArray(int[,] matr)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        array[i, j] = new Random().Next(0, 10);
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); // заполняем числами от 0 до 9
+        }
     }
 }
-PrintArray(array);
-Console.WriteLine();
 
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
+Console.WriteLine("Введите количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+int sum = 0;
+int size = 0;
+
+if (m >= n) size = n;
+else size = m;
+for (int i = 0; i < size; i++)
     {
-        if (i == j) a = a + array[i, j];
+        sum += array[i, i];
     }
-}
-Console.WriteLine(a);
+Console.WriteLine(sum);
