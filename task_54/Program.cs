3b52f89e8ad60e112afb51e3aafe0ea06589c868
Александр Васильев <aleksandrvasilev@Mac-mini-Aleksandr.local>
2022-02-21 11:28:58 +0300

    wrote task_54

---
+++ task_54/Program.cs
@@ -0,0 +1,37 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+int n = new Random().Next(5, 10);
+int k = new Random().Next(5, 10);
+int[,] array = new int[n, k];
+int a = 0;
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+    }
+}
+PrintArray(array);
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (i == j) a = a + array[i, j];
+    }
+}
+Console.WriteLine(a);
